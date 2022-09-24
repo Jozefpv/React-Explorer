215b5c1171ec5fca2b91f56cba60e3df639267fd
Jozefpv <jozefpv99@gmail.com>
2022-09-24 22:56:44 +0100

    arreglo

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -24,11 +24,14 @@ export default function Home(){
           <form onSubmit={handleSubmit}>
             <input placeholder="Buscar..." onChange={handleChange} type='text' value={keyword} />
         </form>
-        <div className="nav">
-          <Link className = "links" to='/gif/camel'>Gif de camellos</Link>
-          <Link className = "links" to='/gif/youtube'>Gif de youtube</Link>
-          <Link className = "links" to='/gif/island'>Gif de islas</Link>
-        </div>
+        {
+            // <div className="nav">
+          //<Link className = "links" to='/gif/camel'>Gif de camellos</Link>
+          //<Link className = "links" to='/gif/youtube'>Gif de youtube</Link>
+          //<Link className = "links" to='/gif/island'>Gif de islas</Link>
+        //</div>
+        }
+       
   
 
         <img className="inicio" src="https://raw.githubusercontent.com/StewartGF/StewartGF/master/images/react.gif"></img>
