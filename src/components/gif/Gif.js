abb562c1bd3bdfd1ae4371b3bea864688ac14b80
Jozefpv <jozefpv99@gmail.com>
2023-05-02 14:17:37 +0100

    añadido

--- src/components/gif/Gif.js
+++ src/components/gif/Gif.js
@@ -4,9 +4,7 @@ export default function Gif({title, url, id}){
     return(
     <a href={`#${url}`} className='Gif'>
         <h4>{title}</h4>
-        {console.log(id)}
-        <small>{id}</small>
-        <img src={url} />
+        <img src={url} alt="foto del gif" />
     </a>
     )
    
